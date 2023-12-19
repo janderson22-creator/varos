a1c45d5f4c9b9bb19fca66df2c420949a7e1f153
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-12-19 16:57:27 -0300

    feat: add logo

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,18 +1,28 @@
+"use client";
+
 import { MenuIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
 import { ModeToggle } from "./toggle-dark-mode";
+import { useTheme } from "next-themes";
+import PetLogoLight from "../../../public/pets-logo-light.png";
+import PetLogoDark from "../../../public/pets-logo-dark.png";
+import Image from "next/image";
 
 const Header = () => {
-    return ( 
-        <Card className="rounded-[0px] flex justify-between p-2">
-            <Button size="icon" variant="outline">
-                <MenuIcon />
-            </Button>
+  const { theme } = useTheme();
 
-            <ModeToggle />
-        </Card>
-    );
-}
- 
+  return (
+    <Card className="flex justify-between rounded-[0px] px-7 py-4">
+      <Button onClick={() => console.log(theme)} size="icon" variant="outline">
+        <MenuIcon />
+      </Button>
+
+      <Image src={theme === "light" ? PetLogoLight : PetLogoDark} alt={"logo"} width={50} className="max-h-[40px]" />
+
+      <ModeToggle />
+    </Card>
+  );
+};
+
 export default Header;
