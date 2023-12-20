8d19e4d67fec0bfb614ec2ade8fcea17d6d63c09
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-12-20 18:13:16 -0300

    feat: auth with google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,6 @@ import { authOptions } from "@/lib/auth"
 import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
+console.log(handler)
 
 export { handler as GET, handler as POST }
