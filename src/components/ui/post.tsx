0eb4d8b4be3ea1862929084c84692504a099d8a9
Janderson Costa <jandersomcosta@yahoo.com.br>
2024-01-11 16:08:38 -0300

    feat: add icons, like and message and show date of post by date-fns

--- src/components/ui/post.tsx
+++ src/components/ui/post.tsx
@@ -2,14 +2,21 @@
 import { Post as PostType } from "@prisma/client";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import DropdownPost from "./dropdown-post";
+import Like from "../svg/like";
+import Comment from "../svg/comment";
+import { formatCreatedAt } from "@/utils/format-date";
 
 interface Props {
   post: PostType;
 }
 
 const Post: React.FC<Props> = ({ post }) => {
+  const formattedDate = formatCreatedAt(post.createdAt);
+
   return (
-    <div className="px-3 py-4">
+    <div
+      className="px-3 py-4"
+    >
       <div className="flex gap-3">
         <Avatar>
           <AvatarFallback>{post.namePet.toUpperCase()}</AvatarFallback>
@@ -26,6 +33,22 @@ const Post: React.FC<Props> = ({ post }) => {
 
       <div className="mt-3">
         <p className="text-sm">{post.text}</p>
+      </div>
+
+      <div className="mt-3 flex items-center gap-2">
+        <div className="flex items-center">
+          <Like liked={true} />
+          <p className="text-xs font-semibold">{post.likes}</p>
+        </div>
+
+        <div className="flex items-center">
+          <Comment />
+          <p className="text-xs font-semibold">0</p>
+        </div>
+      </div>
+
+      <div className="mt-2">
+        <p className="text-[#A09BA3] font-sm">{formattedDate}</p>
       </div>
     </div>
   );
