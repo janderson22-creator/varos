f1bce11604a3b9412e5430ad3d654e7e25e76fb2
Janderson Costa <jandersomcosta@yahoo.com.br>
2024-01-19 14:23:12 -0300

    feat/navbar-mobile

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,9 +3,10 @@
 import { MenuIcon } from "lucide-react";
 import { Button } from "./button";
 import { ModeToggle } from "./toggle-dark-mode";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Sheet, SheetTrigger } from "./sheet";
 import Link from "next/link";
 import Logo from "../svg/logo";
+import NavBar from "./modal-nav-mobile";
 
 const Header = () => {
   return (
@@ -23,13 +24,7 @@ const Header = () => {
           </Button>
         </SheetTrigger>
 
-        <SheetContent side="right">
-          <SheetHeader className="text-left text-lg font-semibold">
-            Menu
-          </SheetHeader>
-
-          <div>teste</div>
-        </SheetContent>
+        <NavBar />
       </Sheet>
     </div>
   );
