61420fc55ffdc48d5f0c06ca2321f5b58bb5ff8b
Janderson Costa <jandersomcosta@yahoo.com.br>
2024-01-20 17:34:28 -0300

    fix: route adjust

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,34 +1,36 @@
-// pages/api/user/route.ts
-
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-interface FormData {
-  name: string;
-  email: string;
-  cellphone: string;
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    try {
-      const formData: FormData = req.body;
-
-      const apiResponse = await fakeApiPost(formData);
-
-      res.status(200).json(apiResponse);
-    } catch (error) {
-      console.error('Error handling POST request:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+export async function POST(request: Request) {
+  if (request.method !== 'POST') {
+    return new Response('Method Not Allowed', {
+      status: 405,
+    });
   }
-}
 
-async function fakeApiPost(formData: FormData) {
-  console.log('Posting to imaginary API:', formData);
-  return { message: 'Post successful!' };
+  const data = await request.json();
+  const requiredFields = [
+    "name",
+    "email",
+    "cellphone",
+  ];
+
+  for (const field of requiredFields) {
+    if (!data[field]) {
+      return new Response(`${field} is a required field.`, {
+        status: 400,
+      });
+    }
+  }
+
+  // Simulando a criação do recurso (pet)
+  const pet = {
+    name: data.name,
+    email: data.email,
+    cellphone: data.cellphone,
+  };
+
+  return new Response(JSON.stringify({ pet }), {
+    status: 201,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
