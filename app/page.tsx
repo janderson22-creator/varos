4e126a14c5ccc134d1b75547462a7de34c55dc80
Janderson Costa <jandersomcosta@yahoo.com.br>
2024-01-20 14:05:13 -0300

    feat: BuyNow season and SimpleInvest season tablet

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-"use client"
+"use client";
 import BuyNow from "@/components/ui/buy-season";
 import Didactics from "@/components/ui/didactics";
 import Footer from "@/components/ui/footer";
 import Form from "@/components/ui/form";
 import Men from "@/components/ui/men";
 import SimpleInvest from "@/components/ui/simple-invest-season";
-import WalletSelect from "@/components/ui/wallet-selection";
+import Cards from "../public/cards-Hero.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div className="mt-[72px]">
+      <div className="mt-[72px] md:flex px-4">
         <BuyNow />
+
+        <div className="hidden md:block">
+          <Image src={Cards} alt={"Cards-Hero"} width={550} />
+        </div>
       </div>
 
       <div className="mt-[164px]">
@@ -30,7 +35,7 @@ export default function Home() {
         <Form />
       </div>
 
-      <div className="px-4 mt-[184px] pb-[48px]">
+      <div className="mt-[184px] px-4 pb-[48px]">
         <Footer />
       </div>
     </div>
